68ae2605dadb9fbf09d8cdce4e8e139bd012f18f
AmanatUMahmud <amanatmahmud@gmail.com>
2022-11-24 08:07:28 +0600

    Footer added

--- src/Pages/Home/Hero.js
+++ src/Pages/Home/Hero.js
@@ -8,10 +8,10 @@ const Hero = () => {
     <div >
     <img src={banner} className="rounded-lg lg:w-[1200px] " alt='Boys and girls of all age reading'/>
     </div>
-    <div className='lg:mx-20'>
+    <div className="lg:mx-20">
       <h1 className="text-5xl font-bold">Want to buy books at low price?</h1>
       <p className="py-6">Book worm provides high quality second hand books at low price. So, that people of all class can earn knowledge and enjoy books.</p>
-      <PrimaryButton>{'Buy now'}</PrimaryButton>
+      <PrimaryButton>{'Discover'}</PrimaryButton>
     </div>
   </div>
 </div>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import AdvertisedItems from './AdvertisedItems';
 import Hero from './Hero';
+import SecondHandProdCategories from './SecondHandProdCategories';
 
 const Home = () => {
     return (
         <div>
             <Hero></Hero>
+            <AdvertisedItems></AdvertisedItems>
+            <SecondHandProdCategories></SecondHandProdCategories>
         </div>
     );
 };
