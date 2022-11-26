b0a87c07e76f0bdb5391249025dbadebf6f3fd3f
AmanatUMahmud <amanatmahmud@gmail.com>
2022-11-26 22:00:44 +0600

    verify implemented

--- src/Pages/DashBoard/Admin/AllBuyer/AllBuyer.js
+++ src/Pages/DashBoard/Admin/AllBuyer/AllBuyer.js
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const AllBuyer = () => {
-    const { data: buyers = [] } = useQuery({
+    const { data: buyers = [] , refetch } = useQuery({
         queryKey: ['buyers'],
         queryFn: async () => {
             const res = await fetch('http://localhost:5000/users?role=user');
@@ -10,6 +10,22 @@ const AllBuyer = () => {
             return data;
         }
     });
+    const handleVerify = (buyer) =>{
+      // console.log(buyer);
+      fetch(`http://localhost:5000/verify?email=${buyer.email}`,{
+        method: 'PUT',
+        headers: {
+          authorization: `bearer ${localStorage.getItem('accessToken')}`
+          },
+    })
+      .then(res=>res.json())
+      .then(data=>{
+        // console.log(data)
+      if(data.acknowledged){
+        refetch()
+      }
+      })
+    }
     return (
         <div className="overflow-x-auto">
         <table className="table table-zebra w-full">
@@ -42,8 +58,9 @@ const AllBuyer = () => {
               </td>
               <td>{buyer.phone ?buyer.phone: "N/A"}</td>
               <td>{buyer.email}</td>
-              <td>{buyer.verified==="no" ?<> <button className="btn btn-xs border-0 bg-[#92B4EC] mr-1">Verify</button><button className="btn btn-xs bg-red-500 border-0">Delete</button></> :
-              <button className="btn btn-xs bg-red-500 border-0">Delete</button>
+              <td>{buyer.verified==="no" ?<> <button className="btn btn-xs border-0 bg-[#92B4EC] mr-1" onClick={()=>handleVerify(buyer)}>Verify</button><button className="btn btn-xs bg-red-500 border-0">Delete</button></> :<><button className="btn btn-xs mr-1" disabled>Verify</button>
+              <button className="btn btn-xs bg-red-500 border-0">Delete</button></>
+              
               
               }</td>
             </tr>)
